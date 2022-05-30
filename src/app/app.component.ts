658652b196ffc297f5dc390bc15d075d30c2e169
johnnytejada <johnnyfran20002@gmail.com>
2022-05-30 19:07:24 -0400

    angular & spring course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './services/employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employee-manager-app';
+export class AppComponent{
+  employees: Employee[] = [];
+  name: string = '';
+  email: string = '';
+  jobTitle: string = '';
+  phone: string = '';
+  imageUrl: string = '';
+
+  constructor(private employeeService: EmployeeService) { } 
+
+  ngOnInit() {
+    this.employeeService.getEmployees().subscribe(
+      (data: Employee[]) => {
+        this.employees = data;
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err.message);
+      }
+    );
+  }
+
+  onAddEmployee(): void {
+    const newEmployee: Employee = {
+      name: this.name,
+      email: this.email,
+      jobTitle: this.jobTitle,
+      phone: this.phone,
+      imageUrl: this.imageUrl
+    };
+    this.employeeService.addEmployee(newEmployee).subscribe(
+      (data: Employee) => {
+        this.employees.push(data);
+        this.name = '';
+        this.email = '';
+        this.jobTitle = '';
+        this.phone = '';
+        this.imageUrl = '';
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err.message);
+      }
+    )
+  }
+
+  onOpenModal(employee: Employee | null, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-bs-toggle', 'modal');
+    if(mode === 'add') {
+      button.setAttribute('data-bs-target', '#addEmployeeModal');
+    }
+    else if(mode === 'edit') {
+      button.setAttribute('data-bs-target', '#editEmployeeModal');
+    }
+    else if(mode === 'delete') {
+      button.setAttribute('data-bs-target', '#deleteEmployeeModal');
+    }
+    container?.appendChild(button);
+    button.click();
+  }
 }
---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Employee } from '../employee';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class EmployeeService {
+    private apiServerUrl = environment.apiBaseURl;
+
+    constructor(private http: HttpClient) { }
+
+    getEmployees(): Observable<Employee[]> {
+        return this.http.get<Employee[]>(this.apiServerUrl + '/employee/all');
+    }
+
+    addEmployee(employee: Employee): Observable<Employee> {
+        return this.http.post<Employee>(this.apiServerUrl + '/employee/add', employee);
+    }
+
+    updateEmployee(employee: Employee): Observable<Employee> {
+        return this.http.put<Employee>(this.apiServerUrl + '/employee/update', employee);
+    }
+
+    deleteEmployee(employeeId: number): Observable<void> {
+        return this.http.delete<void>(this.apiServerUrl + '/employee/delete/' + employeeId);
+    }
+
+}
